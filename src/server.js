3cf4c816fc5e498afca6b6337ad4e7e1a583670d
Jackson Correa <jacksoncorrea@outlook.com.br>
2025-02-22 12:56:39 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import bodyParser from "body-parser";
+import crypto from "crypto";
+import { createClient } from '@supabase/supabase-js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const secret = process.env.SECRET_KW_TOKEN;
+// Configurando Supabase
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+
+// Middleware para processar JSON
+app.use(express.json());
+
+// Rota para o webhook
+app.all('/webhook', bodyParser.raw({ type: 'application/json' }),
+    async (req, res) => {
+        // handle HEAD request
+        if (req.method !== 'POST') {
+            return res.status(200).send({ status: 'ok' });
+        }
+        // receive order's data
+        let order = {};
+        try {
+            order = JSON.parse(req.body);
+        } catch (error) {
+            return res.status(400).send({ error });
+        }
+
+        // verify signature
+        const { signature } = req.query;
+        const calculatedSignature = crypto.createHmac('sha1', secret)
+            .update(JSON.stringify(order)).digest('hex');
+        if (signature !== calculatedSignature) {
+            return res.status(400).send({ error: 'Incorrect signature' });
+        }
+
+        // do any job with received order's data
+        console.log('Received order:', order);
+
+        // return success response
+        return res.status(200).send({ status: 'ok' })
+    });
+// Iniciando o servidor
+app.listen(PORT, () => {
+    console.log(`🚀 Webhook rodando em http://localhost:${PORT}`);
+});
