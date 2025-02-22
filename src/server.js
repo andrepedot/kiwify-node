0830f4084ff5f6a100ada1b3b950772806e4a0ce
andrepedot <andrepedotmartins@gmail.com>
2025-02-22 14:21:13 -0300

    main

--- src/server.js
+++ src/server.js
@@ -14,33 +14,32 @@ const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY
 
 // Middleware para processar JSON
 app.use(express.json());
-
+app.use(bodyParser.json());
 // Rota para o webhook
-app.post('/webhook', bodyParser.raw({ type: 'application/json' }),
-    async (req, res) => {
-        console.log("BODY: ", req.body)
-        // receive order's data
-        // let order = {};
-        // try {
-        //     order = JSON.parse(req.body);
-        // } catch (error) {
-        //     return res.status(400).send({ error });
-        // }
+app.post('/webhook', async (req, res) => {
+    console.log("BODY: ", req.body)
+    // receive order's data
+    // let order = {};
+    // try {
+    //     order = JSON.parse(req.body);
+    // } catch (error) {
+    //     return res.status(400).send({ error });
+    // }
 
-        // // verify signature
-        // const { signature } = req.query;
-        // const calculatedSignature = crypto.createHmac('sha1', secret)
-        //     .update(JSON.stringify(order)).digest('hex');
-        // if (signature !== calculatedSignature) {
-        //     return res.status(400).send({ error: 'Incorrect signature' });
-        // }
+    // // verify signature
+    // const { signature } = req.query;
+    // const calculatedSignature = crypto.createHmac('sha1', secret)
+    //     .update(JSON.stringify(order)).digest('hex');
+    // if (signature !== calculatedSignature) {
+    //     return res.status(400).send({ error: 'Incorrect signature' });
+    // }
 
-        // do any job with received order's data
-        console.log('Received order:', order);
+    // do any job with received order's data
+    // console.log('Received order:', order);
 
-        // return success response
-        return res.status(200).send({ status: 'ok' })
-    });
+    // return success response
+    return res.status(200).send({ status: 'ok' })
+});
 // Iniciando o servidor
 app.listen(PORT, () => {
     console.log(`🚀 Webhook rodando em http://localhost:${PORT}`);
